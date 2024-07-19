4f63b7f47d0b07eadd50c1e70f0c82c92ba54bca
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-07-19 18:22:19 +0800

    Created the tailwind css code for the dashboard.html. Also added a partials/_home_navbar.html. Changed the views function from HTTPResponse to render. Also, added templates folder to the TEMPLATES DIRS.

--- customer_management_system/accounts/views.py
+++ customer_management_system/accounts/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("Home Page")
+    return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return HttpResponse("Products Page")
+    return render(request, 'accounts/products.html')
 
 def customer(request):
-    return HttpResponse("Customer Information Page")
+    return render(request, 'accounts/customer.html')
