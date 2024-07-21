d23348c227002a0b1db78510df19b6e463b0e5be
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-07-21 19:12:55 +0800

    Added static folder and implement images in html pages

--- customer_management_system/customer_management_system/settings.py
+++ customer_management_system/customer_management_system/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Always put this line of code to ensure that Django knows where to find the static files (CSS, JavaScript, Images)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
