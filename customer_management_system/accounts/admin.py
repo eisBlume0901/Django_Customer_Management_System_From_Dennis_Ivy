3b1d82c0a68958edd9a46786d337e469b2c18687
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-07-21 20:44:31 +0800

    Added models and make migrations and created new database tables for customer, product, and order (for order, needs to add more info by establishing relationships)

--- customer_management_system/accounts/admin.py
+++ customer_management_system/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Customer) # This will be present in the admin panel
+admin.site.register(Product)
+admin.site.register(Order)
--- customer_management_system/accounts/models.py
+++ customer_management_system/accounts/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    # For Laravel, this is added in migrations file
+    # And the model, only includes the connection of foreign key and sql statement
+
+    # str is for user friendly representation of the object
+    def __str__(self) -> str:
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor'),
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    # choices gives you a default dropdown
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Order(models.Model):
+    # Enums but Tuples in Django
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
+    )
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
