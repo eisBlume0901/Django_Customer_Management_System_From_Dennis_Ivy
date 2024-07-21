38f2ea951d5a94688e8e74c2324f9ddcb1e986a3
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-07-21 19:29:00 +0800

    Moved images under static folder and added MEDIA_URL in settings.py

--- customer_management_system/customer_management_system/settings.py
+++ customer_management_system/customer_management_system/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_URL = '/images/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
