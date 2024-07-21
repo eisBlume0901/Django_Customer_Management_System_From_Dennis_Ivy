6ccb4a7ed877b8d4d811c0da79848476d614b447
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-07-21 17:26:05 +0800

    Created template layouts for each html pages. Added also icons and logo for the website.

--- customer_management_system/customer_management_system/urls.py
+++ customer_management_system/customer_management_system/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
 
-    path('', include('accounts.urls')),
+    path('', include('accounts.urls')), 
 
     path('admin/', admin.site.urls),
 ]
