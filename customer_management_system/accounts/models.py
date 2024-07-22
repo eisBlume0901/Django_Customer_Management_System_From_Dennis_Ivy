2a37e8e4f53f2a7faa61c50ac84198d9c758cc0b
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-07-22 21:18:12 +0800

    Adjusted some models by adding __str__ method.

--- customer_management_system/accounts/models.py
+++ customer_management_system/accounts/models.py
@@ -46,6 +46,8 @@ class Order(models.Model):
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
 
+    def __str__(self) -> str:
+        return self.product.name + " ordered by " + self.customer.name
 
 class Tag(models.Model):
 
