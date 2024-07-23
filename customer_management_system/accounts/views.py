e1109b7632632f9e2104b7110fabb50fc1a1d259
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-07-23 15:00:03 +0800

    Allowed text wrapping by setting the table attribute to table-fixed and setting the width

--- customer_management_system/accounts/views.py
+++ customer_management_system/accounts/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    allOrders = Order.objects.all()
+    allCustomers = Customer.objects.all()
+
+    context = {
+        'orders': allOrders,
+        'customers': allCustomers
+    }
+
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    allProducts = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': allProducts})
 
 def customer(request):
     return render(request, 'accounts/customer.html')
