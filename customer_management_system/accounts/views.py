00228969fcf4c893c331f622f4a749b1e09cd414
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-07-23 18:29:22 +0800

    Render data to templates (orders, customers, order_counts)

--- customer_management_system/accounts/views.py
+++ customer_management_system/accounts/views.py
@@ -5,10 +5,18 @@ from .models import *
 def home(request):
     allOrders = Order.objects.all()
     allCustomers = Customer.objects.all()
+    allPendingOrders = Order.objects.filter(status="Pending").count()
+    allOutOfDeliveryOrders = Order.objects.filter(status="Out for delivery").count()
+    allDeliveredOrders = Order.objects.filter(status="Delivered").count()
+    allOrdersCount = Order.objects.count()
 
     context = {
         'orders': allOrders,
-        'customers': allCustomers
+        'ordersCount': allOrdersCount,
+        'customers': allCustomers,
+        'pendingOrders': allPendingOrders,
+        'outOfDeliveryOrders': allOutOfDeliveryOrders,
+        'deliveredOrders': allDeliveredOrders,
     }
 
     return render(request, 'accounts/dashboard.html', context)
